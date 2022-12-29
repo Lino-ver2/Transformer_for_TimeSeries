66645bbbcc53434ecff6b8c08de57f8e195e731c
Lino-ver2 <86415531+Lino-ver2@users.noreply.github.com>
2022-12-29 22:35:18 +0900

    add

---
+++ module/lino_module/model.py
@@ -0,0 +1,144 @@
+import time
+import math
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.utils.data import DataLoader
+
+
+class TransformerModel(nn.Module):
+    """Trasnsormer for Time Series
+        参考論文: https://arxiv.org/abs/2001.08317
+    """
+    def __init__(self, d_model: int, nhead: int):
+        super(TransformerModel, self).__init__()
+
+        self.positional = PositionalEncoding(d_model)
+
+        encoder_layer = nn.TransformerEncoderLayer(
+                                                d_model,
+                                                nhead,
+                                                dropout=0.2,
+                                                batch_first=True
+                                                )
+        self.transformer_encoder = nn.TransformerEncoder(
+                                                    encoder_layer,
+                                                    num_layers=4
+                                                    )
+
+        decoder_layer = nn.TransformerDecoderLayer(
+                                                d_model,
+                                                nhead=8,
+                                                dropout=0.2,
+                                                batch_first=True
+                                                )
+        self.transformer_decoder = nn.TransformerDecoder(
+                                                decoder_layer,
+                                                num_layers=4
+                                                )
+        self.linear = nn.Linear(d_model, 1)
+
+    def forward(self, src: Tensor, tgt: Tensor) -> Tensor:
+        # Decoder用のtgt_maskを作成
+        if tgt.dim() == 2:
+            tgt_seq, _ = tgt.shape
+        if tgt.dim() == 3:
+            _, tgt_seq, _ = tgt.shape
+        tgt_mask = _generate_mask(tgt_seq)   # A-look ahead mask
+
+        # Positional Encoding
+        src = self.positional(src)
+        # Encoder
+        memory = self.transformer_encoder(src)
+        # Decoder
+        output = self.transformer_decoder(tgt, memory, tgt_mask)
+        # 線形変で出力の形状へ
+        pred = self.linear(output)
+        return pred
+
+
+def _generate_mask(tgt_seq: int) -> Tensor:
+    """デコーダ入力用の Self Attention用のマスクを作成"""
+    inf_mask = torch.ones(tgt_seq, tgt_seq) * float('-inf')
+    tgt_mask = torch.triu(inf_mask, diagonal=1)
+    return tgt_mask
+
+
+class PositionalEncoding(nn.Module):
+    """PositionalEnoder"""
+    def __init__(self, d_model: int, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = x + self.pe[: x.size(0), :]
+        return x
+
+
+def training(model: object,
+             dataset: DataLoader,
+             device: torch.device,
+             criterion: object,
+             optimizer: object,
+             epochs: int,
+             verbose=10,
+             center=80) -> Tuple[object, Tensor, Tensor]:
+    train_loss = []
+    validation_loss = []
+    print(' start '.center(center, '-'))
+    start_point = time.time()
+    for epoch in range(epochs):
+        epoch_point = time.time()
+        train_epoch_loss = []
+        validation_epoch_loss = []
+
+        cache = None
+        for i, pack in enumerate(dataset):
+            src, tgt, y = [content.to(device) for content in pack]
+            # モデル訓練
+            if i == 0:
+                pass
+            else:
+                # キャッシュから１バッチ前のデータで訓練
+                cached_src, cached_tgt, cached_y = cache
+                model.train()
+                optimizer.zero_grad()
+                output = model(cached_src, cached_tgt)
+                loss = criterion(output[:, 1, :], cached_y)
+                train_epoch_loss.append(loss.item())
+                # 勾配計算
+                loss.backward()
+                optimizer.step()
+            # モデル評価
+            model.eval()
+            output = model(src, tgt)
+            loss = criterion(output[:, 1, :], y)
+            validation_epoch_loss.append(loss.item())
+            # データをキャッシュに保存して次回の訓練データにする
+            cache = (src, tgt, y)
+
+        validation_loss.append(validation_epoch_loss)
+        train_loss.append(train_epoch_loss)
+
+        if epoch % verbose == 0:
+            print(f' epoch_{epoch} '.center(center))
+            train_mean = torch.mean(torch.tensor(train_epoch_loss)).item()
+            test_mean = torch.mean(torch.tensor(validation_epoch_loss)).item()
+            print('train_loss: ', round(train_mean, 4),
+                  '| validation_loss: ', round(test_mean, 4),
+                  '| time: ', round(time.time() - epoch_point, 3))
+
+    print(' complete!! '.center(center, '-'))
+    print(f'Execution_time: {round(time.time() - start_point, 3)}')
+    return model, torch.tensor(train_loss), torch.tensor(validation_loss)
---
+++ module/lino_module/preprocess.py
@@ -0,0 +1,132 @@
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+
+
+# GPU もしくは CPU の選択
+def select_device():
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print('cuda is selected as device!')
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+        print('mps is selected as device!')
+    else:
+        device = torch.device('cpu')
+    return device
+
+
+# データセットの main関数
+def time_series_dataset(data: pd.DataFrame,
+                        trg_column='item_cnt_day',
+                        seq=7,
+                        d_model=32,
+                        dilation=1,
+                        src_tgt_seq=(6, 2),
+                        batch_size=64) -> Tuple[DataLoader]:
+    data = getattr(_mode_of_freq(data), trg_column)
+    data = StandardScaler().fit_transform(data.values.reshape(-1, 1))
+    data = data.reshape(-1)
+    x, y = _expand_and_split(data, seq)
+    tded, label = _time_delay_embedding(x, y, d_model, dilation)
+    src, tgt = _src_tgt_split(tded, *src_tgt_seq)
+    train, test = _to_torch_dataset(src, tgt, label, batch_size)
+    return train, test
+
+
+def _mode_of_freq(data: pd.DataFrame,
+                  key='date',
+                  freq='D',
+                  mode='sum'
+                  ) -> pd.DataFrame:
+    """時系列データを基本統計量で統合する
+    引数:
+        data: 対象を含むオリジナルデータ
+        key: 時間軸のカラム名
+        freq: グループ単位（D: 日ごと, M: 月ごと, Y: 年ごと）
+        mode: 統計量（sum, mean, etc）
+    """
+    # 日付をobjectからdate_time型に変更
+    data[key] = pd.to_datetime(data[key], format=('%d.%m.%Y'))
+    # 時系列(key)についてグループ単位(freq)の売上数の基本統計量(mode)で出力
+    mode_of_key = getattr(data.groupby(pd.Grouper(key=key, freq=freq)), mode)
+    return mode_of_key()
+
+
+def _expand_and_split(ds: pd.Series, seq: int) -> Tuple[np.ndarray]:
+    """2次元にd_modelずらしたデータと正解データを作成する
+    引数:
+        ds: 単変量時系列データ
+        seq: transformerのシーケンス
+    """
+    endpoint = len(ds) - (seq + 1)
+    expanded = np.stack([ds[i: i + seq + 1] for i in range(0, endpoint)])
+    x = expanded[:, :-1]
+    y = expanded[:, -1]
+    return x, y  # ,expanded  # 挙動の確認用
+
+
+def _time_delay_embedding(x: np.ndarray,
+                          y: np.ndarray,
+                          d_model=32,
+                          dilation=1) -> Tuple[np.ndarray]:
+    """Time Delay Embedding
+    引数:
+        x: 訓練データ
+        y: 正解データ
+        d_model: エンべディング次元数
+        dilation: エンべディングの膨張率
+    """
+    endpoint = x.shape[0] - d_model * dilation
+    span = d_model * dilation
+
+    tded = [x[i: i + span: dilation, :].T for i in range(endpoint)]
+    y = y[span - dilation:]
+    return np.array(tded), np.array(y)
+
+# # time_delay_embeddingの挙動確認用（メモ）
+# i = 0
+# print(expanded[i: i + span: dilation, :][-1,   -2:])
+# print(tded[i][-1, -1], y_[i])
+
+
+def _src_tgt_split(tded: np.ndarray,
+                   src_seq: int,
+                   tgt_seq: int) -> Tuple[np.ndarray]:
+    """エンコーダ入力とデコーダ入力への分割"""
+    src = tded[:, :src_seq]
+    tgt = tded[:, -tgt_seq:]
+    return src, tgt
+
+
+def _to_torch_dataset(src: np.ndarray,
+                      tgt: np.ndarray,
+                      label: np.ndarray,
+                      batch_size: int,
+                      train_rate=0.9) -> DataLoader:
+    """Pytorch用のデータセットへの変換
+    引数:
+        src: エンコーダ入力データ
+        tgt: デコーダ入力データ
+        label: 正解データ
+        batch_size: ミニバッチのバッチサイズ
+    """
+    label = label.reshape(-1, 1)[:len(src)]
+    pack = (src, tgt, label)
+    train_pack = [
+        torch.from_numpy(i.astype(np.float32)).clone()[:int(len(src) * train_rate)]
+        for i in pack
+        ]
+    test_pack = [
+        torch.from_numpy(i.astype(np.float32)).clone()[int(len(src) * train_rate):]
+        for i in pack
+        ]
+    train = TensorDataset(*train_pack)
+    train = DataLoader(train, batch_size, shuffle=False)
+    test = TensorDataset(*test_pack)
+    test = DataLoader(test, batch_size=1, shuffle=False)
+    return train, test
